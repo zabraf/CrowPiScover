1f9fc949930ed6f6191bb40853f021a5dbd78614
Martig <joey.mrtg@eduge.ch>
2019-12-11 16:04:39 +0100

    Ajout de la maquette et tutoriel minecraft

--- test.py
+++ test.py
@@ -4,5 +4,8 @@ fenetre = Tk()
 
 label = Label(fenetre, text="Hello World")
 label.pack()
+Canvas(fenetre, width=250, height=100, bg='ivory').pack(side=TOP, padx=5, pady=5)
+Button(fenetre, text ='Bouton 1').pack(side=LEFT, padx=42, pady=5)
+Button(fenetre, text ='Bouton 2').pack(side=RIGHT, padx=5, pady=5)
 
 fenetre.mainloop()
