3dd3466ab50b1765c308c8fadc685c775ba103e1
Martig <joey.mrtg@eduge.ch>
2020-01-22 15:33:36 +0100

    creating button images and placing them

--- componentClass.py
+++ componentClass.py
@@ -1,12 +1,16 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 class Component:
     def Display(self):
-        componentWindow = Tk()
-        label = Label(componentWindow, text="Hello World")
-        label.pack()
-        Canvas(componentWindow, width=1024, height=600, bg='ivory').pack(side=TOP, padx=5, pady=5)
+        componentWindow = Toplevel()
         Button(componentWindow, text ='Bouton 1').pack(side=LEFT, padx=42, pady=5)
         Button(componentWindow, text ='Go Back', command=componentWindow.destroy).pack(side=RIGHT, padx=5, pady=5)
+
+        bgImage = ImageTk.PhotoImage(Image.open("images/CrowPiDessin.jpg")) # Create a render of the background image
+
+        lblImage = Label(componentWindow, image=bgImage) # Create a label on the root form, the picture and the height
+        lblImage.place(x=-1, y=-1) # Change the position of the label
+
         componentWindow.attributes("-fullscreen", True) 
         componentWindow.mainloop()
