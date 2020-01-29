fe6792b5b179f59320991e1bfafbd7671fabef09
Martig <joey.mrtg@eduge.ch>
2020-01-29 13:57:02 +0100

    Python file and pyhton reading added

--- componentClass.py
+++ componentClass.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import json
 
 class Component:
     def Display(self):
@@ -12,5 +13,15 @@ class Component:
         lblImage = Label(componentWindow, image=bgImage) # Create a label on the root form, the picture and the height
         lblImage.place(x=-1, y=-1) # Change the position of the label
 
+        #Get the jsonfile Datas
+        with open('componentsData.txt') as json_file:
+            data = json.load(json_file)
+            dataString = ""
+            for i in data:#Go through all the components
+                if i == "cmpF":  #If it reachs the searched component
+                    for j in data['cmpF']: #Go through a component datas
+                        print(j['name']) #Display what is needed
+                        lbl = Label(componentWindow, text=j['name'])
+                        lbl.place(x=500, y=600)
         componentWindow.attributes("-fullscreen", True) 
         componentWindow.mainloop()
