d7ed6dcdf449e0b5f237a96adeb0df3391e97188
Martig <joey.mrtg@eduge.ch>
2020-02-05 15:01:08 +0100

    Component are charged dynamically. Images name modified.

--- componentClass.py
+++ componentClass.py
@@ -6,12 +6,9 @@ class Component:
     def Display(self, componentName):
         componentWindow = Toplevel() 
 
-        bgImage = ImageTk.PhotoImage(Image.open("images/CrowPiDessin.jpg")) # Create a render of the background image
+        #componentWindow.configure(background='white')
 
-        lblImage = Label(componentWindow, image=bgImage) # Create a label on the root form, the picture and the height
-        lblImage.place(x=-1, y=-1) # Change the position of the label
-
-        Button(componentWindow, text ='Go Back', command=componentWindow.destroy).place(x=475, y=50)
+        Button(componentWindow, text ='Go Back', command=componentWindow.destroy).place(x=510, y=50)
 
         #Get the jsonfile Datas
         with open('componentsData.txt') as json_file:
@@ -20,29 +17,33 @@ class Component:
             for i in data:#Go through all the components
                 if i == componentName:  #If it reachs the searched component
                     component = data[componentName] #Go through a component datas
-                    #Display what is needed
+                    #Display the componenent name
                     lblName = Label(componentWindow, text=component['Name'], font=("courrier", 25))
                     lblName.place(x=30, y=50)
                     
+                    #Display the description of the component
                     lblDescription = Label(componentWindow, text=component['Description'], font=("courrier", 12), wraplength=300)
                     lblDescription.place(x=30, y=100)
                     
+                    #Display "Fonctionnement"
                     lblFunctionnementTitle = Label(componentWindow, text="Fonctionnement", font=("courrier", 25))
                     lblFunctionnementTitle.place(x=725, y=50)
 
+                    #Display the componenent functionnement
                     lblFunctionnement = Label(componentWindow, text=component['Functionnement'], font=("courrier", 12))
                     lblFunctionnement.place(x=725, y=100)
                    
+                    #Display the picture of the element
                     global componentImage
-                    componentImage = ImageTk.PhotoImage(file = r"images/boutonPad.jpg")
+                    componentImage = ImageTk.PhotoImage(file = r"images/"+component['ImageLink'])
                     lblImage = Label(componentWindow, image=componentImage) # Create a label on the root form, the picture and the height
                     lblImage.place(x=480, y=250) # Change the position of the label
 
-                    Button(componentWindow, text ='Go Back', command=componentWindow.destroy).place(x=475, y=50)
-
+                    #Display the pins image/value
                     lblPins = Label(componentWindow, text=component['Pins'], font=("courrier", 20)) #Should be a picture
-                    lblPins.place(x=480, y=450)
-
+                    lblPins.place(x=490, y=450)
+                    
+                    #Display Test componenent button
                     Button(componentWindow, text ='Test/Description technique', command=componentWindow.destroy).place(x=450, y=500)
         componentWindow.attributes("-fullscreen", True) 
         componentWindow.mainloop()
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import componentClass
 
 class Main: 
     def DisplayButton(window, o):
-        def Callback():
-            o.Display("cmpF")
+        def Callback(component):
+            o.Display(component)
         # Garbage collector delete it, if it's not global.
         global imageBoutonPad
         global imageBoutonsGrid
@@ -29,51 +29,51 @@ class Main:
         global imageUltraSound
         global imageVibrator
 
-        imageBoutonPad = ImageTk.PhotoImage(file = r"images/boutonPad.jpg")
-        imageBoutonsGrid = ImageTk.PhotoImage(file = r"images/boutonsGrille.jpg")
+        imageBoutonPad = ImageTk.PhotoImage(file = r"images/ButtonsPad.jpg")
+        imageBoutonsGrid = ImageTk.PhotoImage(file = r"images/ButtonsGrid.jpg")
         imageBreadBoard = ImageTk.PhotoImage(file = r"images/BreadBoard.jpg")
         imageBreadBoardC = ImageTk.PhotoImage(file = r"images/BreadBoardC.jpg")
-        imageBuzzer = ImageTk.PhotoImage(file = r"images/buzzer.jpg")
-        imageHumidityCaptor = ImageTk.PhotoImage(file = r"images/capteurHumidite.jpg")
-        imageDigitScreen = ImageTk.PhotoImage(file = r"images/ecranDigit.jpg")
+        imageBuzzer = ImageTk.PhotoImage(file = r"images/Buzzer.jpg")
+        imageHumidityCaptor = ImageTk.PhotoImage(file = r"images/HumidityCaptor.jpg")
+        imageDigitScreen = ImageTk.PhotoImage(file = r"images/DigitScreen.jpg")
         imageIR = ImageTk.PhotoImage(file = r"images/IR.jpg")
         imageLCD = ImageTk.PhotoImage(file = r"images/LCD.jpg")
         imageLedMatrix = ImageTk.PhotoImage(file = r"images/LedMatrix.jpg")
-        imageLight = ImageTk.PhotoImage(file = r"images/light.jpg")
-        imageMotion = ImageTk.PhotoImage(file = r"images/motion.jpg")
-        imageRaspBerry = ImageTk.PhotoImage(file = r"images/RaspberryDessin.jpg")
+        imageLight = ImageTk.PhotoImage(file = r"images/Light.jpg")
+        imageMotion = ImageTk.PhotoImage(file = r"images/Motion.jpg")
+        imageRaspBerry = ImageTk.PhotoImage(file = r"images/Raspberry.jpg")
         imageRFID = ImageTk.PhotoImage(file = r"images/RFID.jpg")
-        imageSound = ImageTk.PhotoImage(file = r"images/sound.jpg")
-        imageStepMotor = ImageTk.PhotoImage(file = r"images/stepMotor.jpg")
-        imageServ = ImageTk.PhotoImage(file = r"images/sever.jpg")
-        imageTouchCaptor = ImageTk.PhotoImage(file = r"images/touchCaptor.jpg")
-        imageUltraSound = ImageTk.PhotoImage(file = r"images/ultrason.jpg")
-        imageVibrator = ImageTk.PhotoImage(file = r"images/vibreur.jpg")
+        imageSound = ImageTk.PhotoImage(file = r"images/Sound.jpg")
+        imageStepMotor = ImageTk.PhotoImage(file = r"images/StepMotor.jpg")
+        imageServ = ImageTk.PhotoImage(file = r"images/Sever.jpg")
+        imageTouchCaptor = ImageTk.PhotoImage(file = r"images/TouchCaptor.jpg")
+        imageUltraSound = ImageTk.PhotoImage(file = r"images/Ultrasound.jpg")
+        imageVibrator = ImageTk.PhotoImage(file = r"images/Vibrator.jpg")
 
-        Button(window, command=Callback, borderwidth=0, image=imageRaspBerry).place(x=75, y=35)
-        Button(window, command=Callback, borderwidth=0, image=imageBoutonPad).place(x=248, y=428)
-        Button(window, command=Callback, borderwidth=0, image=imageBoutonsGrid).place(x=390, y=428)
-        Button(window, command=Callback, borderwidth=0, image=imageBreadBoard).place(x=455, y=173)
-        Button(window, command=Callback, borderwidth=0, image=imageBreadBoardC).place(x=580, y=428)
-        Button(window, command=Callback, borderwidth=0, image=imageBuzzer).place(x=892, y=314)
-        Button(window, command=Callback, borderwidth=0, image=imageHumidityCaptor).place(x=648, y=428)
-        Button(window, command=Callback, borderwidth=0, image=imageDigitScreen).place(x=775, y=173)
-        Button(window, command=Callback, borderwidth=0, image=imageIR).place(x=710, y=428)
-        Button(window, command=Callback, borderwidth=0, image=imageLCD).place(x=456, y=38)
-        Button(window, command=Callback, borderwidth=0, image=imageLedMatrix).place(x=618, y=173)
-        Button(window, command=Callback, borderwidth=0, image=imageLight).place(x=775, y=316)
-        Button(window, command=Callback, borderwidth=0, image=imageMotion).place(x=775, y=392)
-        Button(window, command=Callback, borderwidth=0, image=imageRFID).place(x=88, y=408)
-        Button(window, command=Callback, borderwidth=0, image=imageSound).place(x=775, y=355)
-        Button(window, command=Callback, borderwidth=0, image=imageStepMotor).place(x=775, y=526)
-        Button(window, command=Callback, borderwidth=0, image=imageServ).place(x=865, y=526)
-        Button(window, command=Callback, borderwidth=0, image=imageTouchCaptor).place(x=648, y=507)
-        Button(window, command=Callback, borderwidth=0, image=imageUltraSound).place(x=775, y=450)
-        Button(window, command=Callback, borderwidth=0, image=imageVibrator).place(x=775, y=245)
+        Button(window, command= lambda: Callback("Raspberry"), borderwidth=0, image=imageRaspBerry).place(x=75, y=35)
+        Button(window, command= lambda: Callback("BoutonPad"), borderwidth=0, image=imageBoutonPad).place(x=248, y=428)
+        Button(window, command= lambda: Callback("ButtonsGrid"), borderwidth=0, image=imageBoutonsGrid).place(x=390, y=428)
+        Button(window, command= lambda: Callback("BreadBoard"), borderwidth=0, image=imageBreadBoard).place(x=455, y=173)
+        Button(window, command= lambda: Callback("BreadBoardC"), borderwidth=0, image=imageBreadBoardC).place(x=580, y=428)
+        Button(window, command= lambda: Callback("Buzzer"), borderwidth=0, image=imageBuzzer).place(x=892, y=314)
+        Button(window, command= lambda: Callback("HumidityCaptor"), borderwidth=0, image=imageHumidityCaptor).place(x=648, y=428)
+        Button(window, command= lambda: Callback("DigitScreen"), borderwidth=0, image=imageDigitScreen).place(x=775, y=173)
+        Button(window, command= lambda: Callback("IR"), borderwidth=0, image=imageIR).place(x=710, y=428)
+        Button(window, command= lambda: Callback("LCD"), borderwidth=0, image=imageLCD).place(x=456, y=38)
+        Button(window, command= lambda: Callback("LedMatrix"), borderwidth=0, image=imageLedMatrix).place(x=618, y=173)
+        Button(window, command= lambda: Callback("Light"), borderwidth=0, image=imageLight).place(x=775, y=316)
+        Button(window, command= lambda: Callback("Motion"), borderwidth=0, image=imageMotion).place(x=775, y=392)
+        Button(window, command= lambda: Callback("RFID"), borderwidth=0, image=imageRFID).place(x=88, y=408)
+        Button(window, command= lambda: Callback("Sound"), borderwidth=0, image=imageSound).place(x=775, y=355)
+        Button(window, command= lambda: Callback("StepMotor"), borderwidth=0, image=imageStepMotor).place(x=775, y=526)
+        Button(window, command= lambda: Callback("Sever"), borderwidth=0, image=imageServ).place(x=865, y=526)
+        Button(window, command= lambda: Callback("TouchCaptor"), borderwidth=0, image=imageTouchCaptor).place(x=648, y=507)
+        Button(window, command= lambda: Callback("Ultrasound"), borderwidth=0, image=imageUltraSound).place(x=775, y=450)
+        Button(window, command= lambda: Callback("Vibrator"), borderwidth=0, image=imageVibrator).place(x=775, y=245)
         return window
     
     root = Tk() # Main form
-    bgImage = Image.open("images/CrowPiDessin.jpg") # Image background
+    bgImage = Image.open("images/CrowPiBG.jpg") # Image background
     render = ImageTk.PhotoImage(bgImage) # Create a render of the background image
 
     lblImage = Label(root, image=render) # Create a label on the root form, the picture and the height
