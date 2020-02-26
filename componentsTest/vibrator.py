c36ef36e64c94c359d7841fc505226eec5ba7d88
zabraf <lopesraphi@gmail.com>
2020-02-26 16:05:53 +0100

    Added Buzzer annd LCD

--- componentsTest/vibrator.py
+++ componentsTest/vibrator.py
@@ -2,17 +2,29 @@
 from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import messagebox
+import RPi.GPIO as GPIO
+import time
+# define vibration pin
+vibration_pin = 27
+
+# Set board mode to GPIO.BOARD
+GPIO.setmode(GPIO.BCM)
+
+# Setup vibration pin to OUTPUT
+GPIO.setup(vibration_pin, GPIO.OUT)
+
 
 def ExecuteExemple():
-    #Do your shit here
-    messagebox.showinfo("BipBip", "I'm a sheep")
+    GPIO.output(vibration_pin, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(vibration_pin, GPIO.LOW)
 
 def app_lost_focus(event): 
     exit()
    
 window = Tk() # Main form
-window.geometry("300x300+400+100")
-Button(window, command= lambda: ExecuteExemple(), text="ON/OFF").place(x=75, y=35)
+window.geometry("400x200+300+200")
+Button(window, command= lambda: ExecuteExemple(), bg="green", activebackground="darkgreen", width="17", height="5").place(x=100, y=50)
 window.bind("<FocusOut>", app_lost_focus)
 window.mainloop() # Load the form 
 
