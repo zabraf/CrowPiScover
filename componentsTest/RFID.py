3b53546a51b1bf30a970669c52fff685a0d84d8a
zabraf <lopesraphi@gmail.com>
2020-03-15 18:09:54 +0100

    finished

--- componentsTest/RFID.py
+++ componentsTest/RFID.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
+import sys
 from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import messagebox
@@ -6,15 +7,20 @@ from threading import Timer
 import RPi.GPIO as GPIO
 import MFRC522
 import signal
+import time
 
 
 
 def app_lost_focus(event):
+    global canrun
+    canrun = False
     t.cancel()
-    GPIO.cleanup()
+    sys.exit("Error message")
     exit()
     
 def on_closing():
+    global canrun
+    canrun = False
     t.cancel()
     GPIO.cleanup()
     exit()
@@ -35,20 +41,25 @@ signal.signal(signal.SIGINT, end_read)
 # create the reader object
 MIFAREReader = MFRC522.MFRC522()
 
-
+canrun = True
 def loop():
-    # detect touch of the card, get status and tag type
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    global canrun
+    if canrun:
+        # detect touch of the card, get status and tag type
+        (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-    # Get the RFID card uid and status
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+        # Get the RFID card uid and status
+        (status,uid) = MIFAREReader.MFRC522_Anticoll()
 
-    # If status is alright, continue to the next stage
-    if status == MIFAREReader.MI_OK:
-        # Print UID
-        lblRFID.config(text="%s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
-    t = Timer(1, loop)
-    t.start()
+        # If status is alright, continue to the next stage
+        if status == MIFAREReader.MI_OK:
+            # Print UID
+            lblRFID.config(text="%s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
+            time.sleep(2)
+        t = Timer(1, loop)
+        t.start()
+    else :
+        exit()
 t = Timer(1, loop)
 t.start()
 
